34a186590fd198cb340a347f0727bdaf2a4307aa
fanning123456 <2408727768@qq.com>
2023-06-07 20:32:08 +0800

    123
    我是范宁 我很帅

--- src/components/body/VideoPlay.vue
+++ src/components/body/VideoPlay.vue
@@ -33,7 +33,7 @@ export default {
                 //获取时间
                 let valueName =
                     (new Date().getFullYear() +
-                    "/" +
+                    "/" +1223+
                     (new Date().getMonth() + 1) +
                     "/" +
                     new Date().getDate() +
